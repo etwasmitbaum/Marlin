83b515f77d7aa503310c8bb2a0f51eefbbe28e2a
etwasmitbaum <47484288+etwasmitbaum@users.noreply.github.com>
2022-11-14 19:23:16 +0100

    fixed e step skipping

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2944,11 +2944,11 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      800
+    #define E0_CURRENT      920
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
-    //#define E0_INTERPOLATE true
+    #define E0_INTERPOLATE false
     //#define E0_HOLD_MULTIPLIER 0.5
   #endif
 
